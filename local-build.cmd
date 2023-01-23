1a23ba639717d5d4767f0007e518a9d4b870ba29
dirkarnez <smalldirkalex@gmail.com>
2023-01-24 01:08:41 +0800

    - add libraries

--- local-build.cmd
+++ local-build.cmd
@@ -4,22 +4,26 @@ cd /d %~dp0
 set DOWNLOADS_DIR=%USERPROFILE%\Downloads
 set DOWNLOADS_DIR_LINUX=%DOWNLOADS_DIR:\=/%
 
-SET PATH=^
-%DOWNLOADS_DIR%\PortableGit\bin;^
-%DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0;^
-%DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\bin;^
-%DOWNLOADS_DIR%\cmake-3.22.2-windows-x86_64\bin;
+@REM SET PATH=^
+@REM %DOWNLOADS_DIR%\PortableGit\bin;^
+@REM %DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0;^
+@REM %DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\bin;^
+@REM %DOWNLOADS_DIR%\cmake-3.22.2-windows-x86_64\bin;
 
-@REM set PATH=^
-@REM D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64;^
-@REM D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin;^
-@REM D:\Softwares\cmake-3.23.0-rc1-windows-x86_64\bin;
+set PATH=^
+D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64;^
+D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin;^
+D:\Softwares\cmake-3.23.0-rc1-windows-x86_64\bin;
+
+@REM -DCURL_DIR="%DOWNLOADS_DIR_LINUX%/curl-v7.86.0-x86_64-posix-seh-rev0-8.1.0/lib/cmake/CURL" ^
 
 cmake.exe -G"MinGW Makefiles" ^
 -DCMAKE_BUILD_TYPE=Debug ^
--DCURL_DIR="%DOWNLOADS_DIR_LINUX%/curl-v7.86.0-x86_64-posix-seh-rev0-8.1.0/lib/cmake/CURL" ^
--B./build &&^
-cd build &&^
+-DPortAudio_DIR="D:/Code Projects/cpp-libraries/portaudio-v19.7.0-mingw64-x86_64-posix-seh-rev0-8.1.0/lib/cmake/portaudio" ^
+-DPortAudioCpp_DIR="D:/Code Projects/cpp-libraries/portaudiocpp-v19.7.0-mingw64-x86_64-posix-seh-rev0-8.1.0" ^
+-DCURL_DIR="D:/Code Projects/cpp-libraries/curl-v7.86.0-x86_64-posix-seh-rev0-8.1.0/lib/cmake/CURL" ^
+-B./cmake-build &&^
+cd cmake-build &&^
 cmake --build . &&^
 echo "Successful build"
 pause
--- src/main.cpp
+++ src/main.cpp
@@ -2,147 +2,121 @@
 #include <stdlib.h>
 #include <string.h>
 #include <iostream>
-#ifndef WIN32
-#  include <unistd.h>
-#endif
-#include <curl/curl.h>
+#include <cmath>
+#include <cassert>
+#include <cstddef>
+#include "portaudiocpp/PortAudioCpp.hxx"
+#include "curl/curl.h"
 
-static const char *urls[] = {
-  "https://www.microsoft.com",
-  "https://opensource.org",
-  "https://www.google.com",
-  "https://www.yahoo.com",
-  "https://www.ibm.com",
-  "https://www.mysql.com",
-  "https://www.oracle.com",
-  "https://www.ripe.net",
-  "https://www.iana.org",
-  "https://www.amazon.com",
-  "https://www.netcraft.com",
-  "https://www.heise.de",
-  "https://www.chip.de",
-  "https://www.ca.com",
-  "https://www.cnet.com",
-  "https://www.mozilla.org",
-  "https://www.cnn.com",
-  "https://www.wikipedia.org",
-  "https://www.dell.com",
-  "https://www.hp.com",
-  "https://www.cert.org",
-  "https://www.mit.edu",
-  "https://www.nist.gov",
-  "https://www.ebay.com",
-  "https://www.playstation.com",
-  "https://www.uefa.com",
-  "https://www.ieee.org",
-  "https://www.apple.com",
-  "https://www.symantec.com",
-  "https://www.zdnet.com",
-  "https://www.fujitsu.com/global/",
-  "https://www.supermicro.com",
-  "https://www.hotmail.com",
-  "https://www.ietf.org",
-  "https://www.bbc.co.uk",
-  "https://news.google.com",
-  "https://www.foxnews.com",
-  "https://www.msn.com",
-  "https://www.wired.com",
-  "https://www.sky.com",
-  "https://www.usatoday.com",
-  "https://www.cbs.com",
-  "https://www.nbc.com/",
-  "https://slashdot.org",
-  "https://www.informationweek.com",
-  "https://apache.org",
-  "https://www.un.org",
+struct MemoryStruct
+{
+  char *memory;
+  size_t size;
 };
-
-#define MAX_PARALLEL 10 /* number of simultaneous transfers */
-#define NUM_URLS sizeof(urls)/sizeof(char *)
 
 static size_t write_cb(char *data, size_t n, size_t l, void *userp)
 {
+  std::cout << "write_cb" << std::endl;
   /* take care of the data here, ignored in this example */
   (void)data;
   (void)userp;
-  return n*l;
-}
-
-static void add_transfer(CURLM *cm, int i)
-{
-  CURL *eh = curl_easy_init();
-  curl_easy_setopt(eh, CURLOPT_WRITEFUNCTION, write_cb);
-  curl_easy_setopt(eh, CURLOPT_URL, urls[i]);
-  curl_easy_setopt(eh, CURLOPT_PRIVATE, urls[i]);
-  curl_multi_add_handle(cm, eh);
-}
-
-
-static void download_page(void)
-{
-    FILE *fp;
-    CURLcode res;
-    std::string url = "https://stackoverflow.com";
-    std::cout << "downloading " << url << std::endl;
-    char outfilename[FILENAME_MAX] = "page.html";
-    CURL *curl = curl_easy_init();                                                                                                                                                                                                                                                           
-    if (curl)
-    {   
-      fp = fopen(outfilename,"wb");
-      curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
-      curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);
-      curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
-      res = curl_easy_perform(curl);
-      curl_easy_cleanup(curl);
-      fclose(fp);
-    }
+  // sendToMP3Decoder(pStreamData, chunkSize)
+  return n * l;
 }
 
 int main(void)
 {
-  CURLM *cm;
-  CURLMsg *msg;
-  unsigned int transfers = 0;
-  int msgs_left = -1;
-  int still_alive = 1;
+  CURL *curl_handle;
+  struct MemoryStruct chunk;
+
+  chunk.memory = NULL; // we expect realloc(NULL, size) to work
+  chunk.size = 0;      // no data at this point
 
   curl_global_init(CURL_GLOBAL_ALL);
-  cm = curl_multi_init();
 
-  /* Limit the amount of simultaneous connections curl should allow: */
-  curl_multi_setopt(cm, CURLMOPT_MAXCONNECTS, (long)MAX_PARALLEL);
+  // init the curl session
+  curl_handle = curl_easy_init();
 
-  for(transfers = 0; transfers < MAX_PARALLEL; transfers++)
-    add_transfer(cm, transfers);
+  // specify URL to get
+  curl_easy_setopt(curl_handle, CURLOPT_URL, "http://bignoise.club:8000/nonstop");
 
-  do {
-    curl_multi_perform(cm, &still_alive);
+  // send all data to this function
+  curl_easy_setopt(curl_handle, CURLOPT_WRITEFUNCTION, write_cb);
 
-    while((msg = curl_multi_info_read(cm, &msgs_left))) {
-      if(msg->msg == CURLMSG_DONE) {
-        char *url;
-        CURL *e = msg->easy_handle;
-        curl_easy_getinfo(msg->easy_handle, CURLINFO_PRIVATE, &url);
-        fprintf(stderr, "R: %d - %s <%s>\n",
-                msg->data.result, curl_easy_strerror(msg->data.result), url);
-        curl_multi_remove_handle(cm, e);
-        curl_easy_cleanup(e);
-      }
-      else {
-        fprintf(stderr, "E: CURLMsg (%d)\n", msg->msg);
-      }
-      if(transfers < NUM_URLS)
-        add_transfer(cm, transfers++);
+  // we pass our 'chunk' struct to the callback function
+  curl_easy_setopt(curl_handle, CURLOPT_WRITEDATA, (void *)&chunk);
+
+  // some servers don't like requests that are made without a user-agent field, so we provide one
+  curl_easy_setopt(curl_handle, CURLOPT_USERAGENT, "libcurl-agent/1.0");
+
+  // don't use signal for timeout
+  curl_easy_setopt(curl_handle, CURLOPT_NOSIGNAL, (long)1);
+
+  // set timeout to 10 seconds
+  curl_easy_setopt(curl_handle, CURLOPT_TIMEOUT, 10);
+
+  // if(strcmp(g_settings.softupdate_proxyserver, "") != 0)
+  // {
+  //   curl_easy_setopt(curl_handle, CURLOPT_PROXY, g_settings.softupdate_proxyserver);
+
+  //   if(strcmp(g_settings.softupdate_proxyusername, "") != 0)
+  //   {
+  //     char tmp[200];
+  //     strcpy(tmp, g_settings.softupdate_proxyusername);
+  //     strcat(tmp, ":");
+  //     strcat(tmp, g_settings.softupdate_proxypassword);
+  //     curl_easy_setopt(curl_handle, CURLOPT_PROXYUSERPWD, tmp);
+  //   }
+  // }
+
+  // get it!
+  curl_easy_perform(curl_handle);
+
+  // cleanup curl stuff
+  curl_easy_cleanup(curl_handle);
+
+  long res_code;
+  if (curl_easy_getinfo(curl_handle, CURLINFO_HTTP_CODE, &res_code) == CURLE_OK)
+  {
+    if (200 == res_code)
+    {
+      // //printf("\nchunk = %s\n", chunk.memory);
+      // std::istringstream iss;
+      // iss.str (std::string(chunk.memory, chunk.size));
+      // char line[512];
+      // char *ptr;
+
+      // while (iss.rdstate() == std::ifstream::goodbit)
+      // {
+      //   iss.getline(line, 512);
+      //   if (line[0] != '#')
+      //   {
+      //     //printf("chunk: line = %s\n", line);
+      //     ptr = strstr(line, "http://");
+      //     if (ptr != NULL)
+      //     {
+      //       char *tmp;
+      //       // strip \n and \r characters from url
+      //       tmp = strchr(line, '\r');
+      //       if (tmp != NULL)
+      //         *tmp = '\0';
+      //       tmp = strchr(line, '\n');
+      //       if (tmp != NULL)
+      //         *tmp = '\0';
+
+      //       addUrl2Playlist(ptr, name, tim);
+      //     }
+      //   }
+      // }
     }
-    if(still_alive)
-      curl_multi_wait(cm, NULL, 0, 1000, NULL);
+  }
 
-  } while(still_alive || (transfers < NUM_URLS));
+  if (chunk.memory)
+  {
+    free(chunk.memory);
+  }
 
-  curl_multi_cleanup(cm);
+  // we're done with libcurl, so clean it up
   curl_global_cleanup();
-
-  download_page();
-
   return EXIT_SUCCESS;
 }
